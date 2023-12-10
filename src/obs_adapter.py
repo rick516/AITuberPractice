92ead51de7fcd0bd548938344824174c433840d2
rick516 <k.murakami516@gmail.com>
2023-12-10 17:39:18 +0900

    fix: クラスメソッドの抽象化 + ロギングの修正

--- src/obs_adapter.py
+++ src/obs_adapter.py
@@ -9,19 +9,15 @@ class OBSAdapter:
         host = os.getenv("OBS_WS_HOST")
         port = os.getenv("OBS_WS_PORT")
         if (password == None or host == None or port == None):
-            raise Exception("OBSの設定がされていません。")
+            raise Exception("OBSの設定がされていません")
         self.ws = obs.ReqClient(host=host, port=port, password=password)
 
-    def set_question(self, text: str):
-        self.ws.set_input_settings(name="Question", settings={"text": text}, overlay=True)
-
-    def set_answer(self, text: str):
-        self.ws.set_input_settings(name="Answer", settings={"text": text}, overlay=True)
-
+    def set_text_source(self, name: str, text: str):
+        self.ws.set_input_settings(name=name, settings={"text": text}, overlay=True)
 
 if __name__ == "__main__":
     obs = OBSAdapter()
     question = input("質問を入力してください: ")
-    obs.set_question(question)
+    obs.set_text_source("Question", question)
     answer = input("回答を入力してください: ")
-    obs.set_answer(answer)
+    obs.set_text_source("Answer", answer)
